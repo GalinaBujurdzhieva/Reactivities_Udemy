22132aff213ea9e6f3801ea0492435f96eacefa7
GalinaBujurdzhieva <111417862+GalinaBujurdzhieva@users.noreply.github.com>
2026-05-04 22:06:50 +0300

    CRUD, Mediator and AutoMapper

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,5 @@
+using Application.Activities.Queries;
+using Application.Core;
 using Microsoft.EntityFrameworkCore;
 using Persistance;
 
@@ -11,6 +13,8 @@ builder.Services.AddDbContext<Persistance.AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
+builder.Services.AddAutoMapper(cfg => cfg.AddProfile<MappingProfiles>());
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 
 var app = builder.Build();
